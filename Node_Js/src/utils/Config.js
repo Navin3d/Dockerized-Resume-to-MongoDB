ca9a1573917c8bc2fc1d0ba52b23bd48045513d1
Navin3d <smnavin65@mail.com>
2023-02-04 11:15:03 +0530

    Added ENV Variables in docker compose

--- Node_Js/src/utils/Config.js
+++ Node_Js/src/utils/Config.js
@@ -2,7 +2,7 @@ const PORTNO = 3001;
 const DATABASE_URL = `mongodb://docker:mongopw@mongodb-server:27017`;
 const BASE_URL = "http://node-js:3001";
 module.exports = {
-    PORTNO,
-    DATABASE_URL,
-    BASE_URL,
+    PORTNO: process.env.PORTNO,
+    DATABASE_URL: process.env.DATABASE_URL,
+    BASE_URL: process.env.BASE_URL,
 };
--- React_Js/src/configs/index.js
+++ React_Js/src/configs/index.js
@@ -1,1 +1,1 @@
-export const BASEURL = "http://localhost:3001";
+export const BASEURL = process.env.BASEURL;
